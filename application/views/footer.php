c3c665bbe343a6097230e0d157b01f944eb917d0
mohsinwarsi1996 <smohsin609@gmail.com>
2021-03-22 17:34:49 +0500

    initial commit

---
+++ application/views/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 3.1.0-rc
+    </div>
+    <strong>Copyright &copy; 2020-2021 <a href="https://adminlte.io">Manzil Admin Panel</a>.</strong> All rights reserved.
+  </footer>
